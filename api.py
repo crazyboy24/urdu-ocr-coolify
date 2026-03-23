e94a28c2ab03f2916c36fe3a456abe6f9a4baa73
crazyboy24 <mamoeid@hotmail.com>
2026-03-23 19:30:18 +0530

    Update api.py

--- api.py
+++ api.py
@@ -160,5 +160,5 @@ def ocr_pdf():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=False, threaded=False)
